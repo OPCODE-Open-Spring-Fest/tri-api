440ffd18bfd0ba6b58d1fdaaf6cc61d27a38d5be
x256 <153709773+xTwo56@users.noreply.github.com>
2024-03-24 17:10:47 +0530

    feat: create carsInstance
    carsInstance will be used in CarsCode.js to fetch data as per user's input

---
+++ React-app/src/Components/Cars/CarsAPI.js
@@ -0,0 +1,12 @@
+
+import axios from 'axios'
+
+const carsInstance = axios.create({
+  baseURL: 'https://car-data.p.rapidapi.com',
+  headers: {
+    'X-RapidAPI-Key': 'fb50c93a1cmsh09e5e23ca2cef98p1aa82fjsncd2ecfe53965',
+    'X-RapidAPI-Host': 'car-data.p.rapidapi.com'
+  },
+})
+
+export default carsInstance
