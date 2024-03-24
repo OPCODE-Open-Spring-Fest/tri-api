446bc4e38b3b0459bc6131f2650a0d58b7ee5045
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2024-03-24 21:54:27 +0530

    Update imdbAPI.js
    url --> baseURL

--- React-app/src/Components/IMDB/imdbAPI.js
+++ React-app/src/Components/IMDB/imdbAPI.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const imdbInstance = axios.create({
-  url: 'https://imdb146.p.rapidapi.com/v1',
+  baseURL: 'https://imdb146.p.rapidapi.com/v1',
   headers: {
     'X-RapidAPI-Key': 'fb50c93a1cmsh09e5e23ca2cef98p1aa82fjsncd2ecfe53965',
     'X-RapidAPI-Host': 'imdb146.p.rapidapi.com'
