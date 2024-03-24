79039bc38e5123878b643d087ae690fd9aa8aafc
Sharvil Lade <sharvillade9917@gmail.com>
2024-03-24 14:54:31 +0530

    chore: updated both the issues

--- React-app/src/App.js
+++ React-app/src/App.js
@@ -1,14 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+
 import NavbarCode from "./Components/Navbar/NavbarCode";
+
 import HomeCode from "./Components/Home/HomeCode";
 import CarsCode from "./Components/Cars/CarsCode";
 import ImdbCode from "./Components/IMDB/ImdbCode";
 import CurrencyCode from "./Components/Currency/CurrencyCode";
 
 import FooterCode from "./Components/Footer/FooterCode";
-import CarsAPI from "./Components/Cars/CarsAPI";
-import CurrencyAPI from "./Components/Currency/CurrencyAPI";
-import ImdbAPI from "./Components/IMDB/ImdbAPI";
 
 function App() {
   return (
@@ -20,12 +19,6 @@ function App() {
         <Route path="/cars" element={<CarsCode />} />
         <Route path="/imdb" element={<ImdbCode />} />
         <Route path="/currency" element={<CurrencyCode />} />
-
-         {/* Added routes in Footer to the API's */}
-
-        <Route path="/carsapi" element={<CarsAPI />} />
-        <Route path="/currencyapi" element={<CurrencyAPI />} />
-        <Route path="/imdbapi" element={<ImdbAPI />} />
 
       </Routes>
       <FooterCode/>
