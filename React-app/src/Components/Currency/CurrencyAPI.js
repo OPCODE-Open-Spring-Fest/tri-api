a2610ee0be427a29a73f3d3f2797cab4c3d39dd0
x256 <153709773+xTwo56@users.noreply.github.com>
2024-03-25 19:35:49 +0530

    feat: create currencyInstance

--- React-app/src/Components/Currency/CurrencyAPI.js
+++ React-app/src/Components/Currency/CurrencyAPI.js
@@ -1,9 +1,13 @@
 
 
-const CurrencyAPI = () => {
-  return (
-    <div>This is Currency API</div>
-  )
-}
+import axios from 'axios'
 
-export default CurrencyAPI
+const currencyInstance = axios.create({
+  baseURL: 'https://currency-converter18.p.rapidapi.com/api/v1',
+  headers: {
+    'X-RapidAPI-Key': 'fb50c93a1cmsh09e5e23ca2cef98p1aa82fjsncd2ecfe53965',
+    'X-RapidAPI-Host': 'currency-converter18.p.rapidapi.com'
+  }
+})
+
+export default currencyInstance
